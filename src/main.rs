26952a257007c155464250b4c79766d4279c94a9
Baitinq <manuelpalenzuelamerino@gmail.com>
2023-02-23 21:49:41 +0100

    Get all valid update commit messages

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ async fn main() {
 
     let client = reqwest::Client::new();
 
-    let package_path = "development/libraries/glib";
+    let package_name = "glib";
+    let package_path = format!("development/libraries/{package_name}");
     let page = 1;
 
     let body = client.get(format!("https://api.github.com/repos/NixOS/nixpkgs/commits?path=pkgs/{package_path}/default.nix&per_page=100&page={page}"))
@@ -23,4 +24,19 @@ async fn main() {
     let json: Value = serde_json::from_str(&body).unwrap();
 
     println!("json = {:?}", json);
+
+    for commit in json.as_array().unwrap() {
+        let message = commit.get("commit").unwrap().get("message").unwrap();
+        let message_split: Vec<&str> = message.as_str().unwrap().split(" ").collect();
+
+        if *message_split.first().unwrap() != format!("{package_name}:") {
+            continue;
+        }
+
+        if *message_split.get(2).unwrap() != "->" {
+            continue;
+        }
+
+        println!("Message: {}", message)
+    }
 }
