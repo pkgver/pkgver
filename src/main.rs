5c87d7abc398e701fd423c09d7847a0c4a8ed324
Baitinq <manuelpalenzuelamerino@gmail.com>
2023-02-26 01:02:47 +0100

    Isolate the version fetching into its own function

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,25 @@ use linked_hash_map::LinkedHashMap;
 use reqwest::header::{self, CONTENT_TYPE};
 use serde_json::Value;
 
-type Version<'a> = &'a str;
-type CommitHash<'a> = &'a str;
+type Version = String;
+type CommitHash = String;
 
 #[tokio::main]
 async fn main() {
+    let package_name = "htop";
+
+    let mut versions: LinkedHashMap<Version, CommitHash> = LinkedHashMap::new();
+    fetch_versions_from_nixpkgs(&mut versions, package_name).await;
+
+    println!("{:#?}", versions);
+}
+
+async fn fetch_versions_from_nixpkgs(
+    versions: &mut LinkedHashMap<Version, CommitHash>,
+    package_name: &str,
+) {
     let client = reqwest::Client::new();
 
-    let package_name = "htop";
     let package_path = format!("tools/system/{package_name}");
     let page = 1;
 
@@ -27,8 +38,6 @@ async fn main() {
 
     let commits = json.as_array().unwrap();
 
-    let mut versions: LinkedHashMap<Version, CommitHash> = LinkedHashMap::new();
-
     //goes backwards
     for i in 0..commits.len() - 1 {
         let message = commits[i].get("commit").unwrap().get("message").unwrap();
@@ -36,8 +45,8 @@ async fn main() {
 
         if commits.len() == 1 {
             versions.insert(
-                message_split.get(3).unwrap(),
-                commits[i].get("sha").unwrap().as_str().unwrap(),
+                message_split.get(3).unwrap().to_string(),
+                commits[i].get("sha").unwrap().to_string(),
             );
             break;
         }
@@ -45,20 +54,16 @@ async fn main() {
         if *message_split.first().unwrap() == format!("{package_name}:")
             && *message_split.get(2).unwrap() == "->"
         {
-            let from_ver = message_split.get(1).unwrap();
-            let to_ver = message_split.get(3).unwrap();
+            let from_ver = message_split.get(1).unwrap().to_string();
+            let to_ver = message_split.get(3).unwrap().to_string();
 
             // if hashmap is empty create latest version (_to_ver) at commits[0]
             if versions.is_empty() {
-                versions.insert(to_ver, commits[0].get("sha").unwrap().as_str().unwrap());
+                versions.insert(to_ver, commits[0].get("sha").unwrap().to_string());
             }
 
             // _from_ver version's commit sha is one before where its updated to _to_ver
-            versions.insert(
-                from_ver,
-                commits[i + 1].get("sha").unwrap().as_str().unwrap(),
-            );
+            versions.insert(from_ver, commits[i + 1].get("sha").unwrap().to_string());
         }
     }
-    println!("{:?}", versions);
 }
