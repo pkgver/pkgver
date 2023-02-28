6a65c6e1f9bbf493f3573191aec90a32a77ac8f4
dsymbol <dsymbol@users.noreply.github.com>
2023-02-28 02:07:59 +0200

    change fzf version selecting order

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use linked_hash_map::LinkedHashMap;
 use reqwest::header::{self, CONTENT_TYPE};
 use serde_json::Value;
-use rust_fzf::fzf_select;
 use std::env;
+use std::io::Write;
+use std::process::{Command, Stdio};
 
 type Version = String;
 type CommitHash = String;
@@ -17,8 +18,8 @@ async fn main() {
 
     let version_keys: Vec<String> = versions.keys().cloned().collect::<Vec<String>>();
     let options = fzf_select(version_keys);
-    println!("{}", options)
-    
+    println!("{}", options);
+    println!("{:#?}", versions.get(&options).unwrap());
     //println!("{:#?}", versions);
 }
 
@@ -113,4 +114,28 @@ async fn get_package_path(client: &reqwest::Client, package_name: &str) -> Strin
     println!("PATH: {}", path);
 
     path.to_string()
+}
+
+// https://github.com/andrewwillette/rust_fzf
+fn fzf_select(fzf_input: Vec<String>) -> String {
+    let mut child = Command::new("fzf")
+        .arg("--reverse")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to spawn child process");
+
+    let mut stdin = child.stdin.take().expect("Failed to open stdin");
+    let mut fzf_in = String::new();
+    for selection in fzf_input {
+        fzf_in.push_str(&selection);
+        fzf_in.push('\n');
+    }
+    stdin
+        .write_all(fzf_in.as_bytes())
+        .expect("Failed to write fzf_input to fzf command stdin");
+    let output = child
+        .wait_with_output()
+        .expect("Failed to read fzf command stdout");
+    String::from(std::str::from_utf8(&output.stdout).unwrap().trim())
 }
